6bf45d89d742fc79dcff70322d078d2649372bbc
Besar Rahmat <besarrahmat007@gmail.com>
2023-08-11 20:37:21 +0700

    Restrict access for specific role

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,38 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 */
+	public function register(): void
+	{
+		//
+	}
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+	/**
+	 * Bootstrap any application services.
+	 */
+	public function boot(): void
+	{
+		// Using closure based composers..
+		View::composer('layouts.dashboard', function ($view) {
+			if (Auth::user()->roles_id == 3) {
+				$test = User::leftJoin('kups_user', 'kups_user.user_id', '=', 'users.id')
+					->where('users.roles_id', '=', 3)
+					->where('users.id', '=', Auth::user()->id)
+					->get(['kups_user.user_id'])
+					->first();
+				View::share('user_id', $test['user_id']);
+			} else {
+				View::share('user_id', Auth::user()->id);
+			}
+		});
+	}
 }
--- resources/views/includes/menu-sidebar.blade.php
+++ resources/views/includes/menu-sidebar.blade.php
@@ -1,26 +1,28 @@
 <aside id="sidebar" class="sidebar">
 
     <ul class="sidebar-nav" id="sidebar-nav">
-        <li class="nav-item">
-            <a class="nav-link collapsed" href={{ url('user') }}>
-                <i class="bi bi-person"></i>
-                <span>User</span>
-            </a>
-        </li><!-- End Users Page Nav -->
+        @if (Auth::user()->roles->code == 'admin')
+            <li class="nav-item">
+                <a class="nav-link collapsed" href={{ url('user') }}>
+                    <i class="bi bi-person"></i>
+                    <span>User</span>
+                </a>
+            </li><!-- End Users Page Nav -->
 
-        <li class="nav-item">
-            <a class="nav-link collapsed" href={{ url('program') }}>
-                <i class="bx bxs-hdd"></i>
-                <span>Program</span>
-            </a>
-        </li><!-- End Program Page Nav -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href={{ url('program') }}>
+                    <i class="bx bxs-hdd"></i>
+                    <span>Program</span>
+                </a>
+            </li><!-- End Program Page Nav -->
 
-        <li class="nav-item">
-            <a class="nav-link collapsed" href={{ url('extra') }}>
-                <i class="bx bxs-server"></i>
-                <span>Master Jenis Asistensi</span>
-            </a>
-        </li><!-- End Master Jenis Asistensi Page Nav -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href={{ url('extra') }}>
+                    <i class="bx bxs-server"></i>
+                    <span>Master Jenis Asistensi</span>
+                </a>
+            </li><!-- End Master Jenis Asistensi Page Nav -->
+        @endif
 
         <li class="nav-item">
             <a class="nav-link collapsed" href={{ url('lembaga-ps') }}>
@@ -34,7 +36,7 @@
                 <i class="bx bxs-book-bookmark"></i>
                 <span>Lembaga KUPS</span>
             </a>
-            <ul id="kups-nav" class="nav-content">
+            <ul id="kups-nav" class="nav-content" @if (Auth::user()->roles->code != 'admin') style="display:none" @endif>
                 <li>
                     <a href="{{ url('lembaga-kups/' . Auth::user()->id . '/pendampingan') }}">
                         <i class="bi bi-circle"></i>
